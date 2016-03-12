5b3c8de457746dd4ec5b12a593700976a11d1f90
Wikel <hola@grr.la>
2016-03-12 06:50:06 +0000

    update user

--- Install-blockAds.sh
+++ Install-blockAds.sh
@@ -3,6 +3,6 @@ echo "Hosts clixsense..."
 cp /etc/hosts ~/
 sudo rm -rf /etc/hosts
 cd ~
-wget https://raw.githubusercontent.com/Wikelx/hosts-clixsense/master/hosts-clixsense
+wget https://raw.githubusercontent.com/Wikel/hosts-clixsense/master/hosts-clixsense
 sudo ln -s ~/hosts-clixsense /etc/hosts
 exit
